eeafcbf99486d2ebe2b78c28a25297d0a88289bd
EC2 Default User <ec2-user@ip-10-0-0-56.eu-west-1.compute.internal>
2015-10-20 12:35:50 +0000

    couple of change

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,11 @@ import logger
 log = logger.create_logger('root_logger')
 log.debug('Logger Created')
 
+import time
+start = int(round(time.time() * 1000))
+
 from aws.s3 import S3
 s3 = S3()
 print(s3.get('hg38.2bit', save_to_file=True, file_path='m.2bit'))
+length = int(round(time.time() * 1000)) - start
+log.debug('Took %s ms' % (length))

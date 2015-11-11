1ec31e72a5444fb2744e4b146bf5dce67301aa8e
Michael O'Neill <michaeloneill94@live.ie>
2015-11-11 23:11:38 +0000

    Cleaned up a bit - but more cleaning is needed

--- app/master/receiver/__init__.py
+++ app/master/receiver/__init__.py
@@ -3,17 +3,19 @@ log = logging.getLogger('root_logger')
 
 from aws.sqs import workers_messaging_queue
 from master.db.models import session, Task, Job, WorkerLog
-from sqlalchemy import and_
 from datetime import datetime
 from master.async_downloader import AsyncDownloader
 
 class Receiver:
+
+    def __init__(self):
+        self.downloader = AsyncDownloader()
+
     def receive(self):
         log.info('Polling workers messaging queue')
         #Async downlaoder to download task outputs
         #as they are reported
-        downloader = AsyncDownloader()
-        downloader.start()
+        self.downloader.start()
         while True:
             #poll queue
             msg=workers_messaging_queue.poll()
@@ -21,34 +23,38 @@ class Receiver:
             log.debug(msg)
             if 'type' in msg:
                 if msg['type'] == 'log':
-                    #add to DB
-                    l=WorkerLog(**msg['data'])
-                    l.date=datetime.strptime(msg['create_time'], '%Y-%m-%d %H:%M:%S.%f')
-                    session.add(l)
-                    session.commit()
-                    #delete message from SQS
-                    workers_messaging_queue.delete_message()
+                    self.log(msg)
                 elif msg['type'] == 'task':
-                    task = session.query(Task).filter(Task.id == msg['data']['id']).first()
-                    if task:
-                        task.status = msg['data']['status']
-                        task.finished = datetime.strptime(msg['create_time'], '%Y-%m-%d %H:%M:%S.%f')
-                        session.commit()
-                        workers_messaging_queue.delete_message()
-                        #download output if it was a successfult task
-                        if task.status == 'completed':
-                            downloader.add('job-'+task.job_id+'/output/'+str(task.task_id))
-                        #check if all tasks are finished
-                        uncompleted_tasks = session.query(Task).filter(and_(Task.job_id == task.job_id, Task.status != 'completed')).count()
-                        if uncompleted_tasks == 0:
-                            job = task.job
-                            if job.final_script:
-                                if job.final_script == 'merge':
-                                    print 'merge task outputs'
-                                else:
-                                    job.execute_final_script()
-                            job.status = 'master script executing'
-                            job.finished = datetime.utcnow()
+                    self.process_task_output(msg)
 
-                            log.info('Job (%s) Completed' % (task.job_id))
-                            break
+    def log(self, msg):
+        #add to DB
+        l=WorkerLog(**msg['data'])
+        l.date=datetime.strptime(msg['create_time'], '%Y-%m-%d %H:%M:%S.%f')
+        session.add(l)
+        session.commit()
+        #delete message from SQS
+        workers_messaging_queue.delete_message()
+
+    def process_task_output(self, msg):
+        task = session.query(Task).filter(Task.id == msg['data']['id']).first()
+        if task:
+            task.status = msg['data']['status']
+            task.finished = datetime.strptime(msg['create_time'], '%Y-%m-%d %H:%M:%S.%f')
+            session.commit()
+            #download output if it was a successful task and job has a final script
+            if task.status == 'completed' and task.job.final_script:
+                self.downloader.add(task.job.id, 'job-'+task.job_id+'/task_output/'+str(task.task_id))
+            if task.status != 'completed':
+                print 'NOOOOOOOOOOOOO!!!!'
+            workers_messaging_queue.delete_message()
+
+            #check if the whole job is completed
+            if task.job.all_tasks_completed():
+                log.info('Tasks Finished for Job <%s>' % task.job.id)
+                task_output_download_queue = None
+                if task.job.final_script:
+                    task_output_download_queue = self.downloader.pause(task.job.id)
+                task.job.finish(task_output_download_queue=task_output_download_queue)
+                if self.downloader.paused:
+                    self.downloader.resume()

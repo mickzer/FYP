88a8d8e4bc9eb0e974eed35c816918ecba04535d
Michael O'Neill <michaeloneill94@live.ie>
2015-11-11 23:10:26 +0000

    AsyncDownloader Fixed - Can be paused but pausing it will make the calling thread wait if it is in the middle of downloading a file. It can also be resumed after

--- app/master/async_downloader/__init__.py
+++ app/master/async_downloader/__init__.py
@@ -1,6 +1,6 @@
 import logging
 log = logging.getLogger('root_logger')
-import threading, global_conf
+import threading, time, random, global_conf
 from Queue import Queue
 from aws.s3 import s3
 
@@ -8,22 +8,60 @@ class AsyncDownloader(threading.Thread):
 
 	def __init__(self):
 		threading.Thread.__init__(self)
-		self.files = Queue()
-		self.run_thread = True
+		self.files = {}
+		#paused flag
+		self.paused = False
+		#paused locking condition
+		self.pause_cond = threading.Condition(threading.Lock())
 
-	def add(self, path):
-		self.files.put(path)
+	def add(self, job_id, path):
+		if job_id not in self.files:
+			self.files[job_id] = Queue()
+		self.files[job_id].put(path)
 		log.info('Queued for Async Download: ' + path)
 
-	def stop(self):
-		self.run_thread = False
+	def pause(self, job_id):
+		self.paused = True
+		#acquire the lock which will make the calling thread
+		#wait until the current download finishes
+		self.pause_cond.acquire()
+		#return the queue of remaining task outputs
+		#to be downloaded for the param job
+		if job_id in self.files:
+			r = self.files[job_id]
+			del self.files[job_id]
+			return r
+		return None
+
+	def resume(self):
+		self.paused = False
+		# Notify so thread will wake after lock released
+		self.pause_cond.notify()
+		# Now release the lock
+		self.pause_cond.release()
 
 	def run(self):
 		log.info('AsyncDownloader Running')
-		while self.run_thread:
-			f = self.files.get()
-			log.info('Async Downloading ' + f)
-			self.download(f)
-
-	def download(self, path):
-		s3.get(path, file_path=global_conf.CWD+path)
+		while True:
+			with self.pause_cond:
+				while self.paused:
+					self.pause_cond.wait()
+				#get all the queues that aren't empty
+				qs = []
+				if self.files:
+					for key in self.files:
+						if not self.files[key].empty():
+							qs.append({key: self.files[key]})
+				if qs:
+					#pick a random queue
+					q = random.choice(qs)
+					#download the next file in the queue
+					job_id = q.keys()[0]
+					f = q[job_id].get()
+					log.info('Async Downloading: ' + f)
+					#download from s3
+					s3.get(f, file_path=global_conf.CWD+f)
+					#delete the key from self.files if queue is empty
+					if q[job_id].empty():
+						del self.files[job_id]
+				time.sleep(0.5)

2f78cbcfc58186b9c5aadebe93615d1f21535382
Michael O'Neill <michaeloneill94@live.ie>
2015-12-02 18:01:40 +0000

    Added new REST endpoint for getting a job's tasks

--- app/master/web_server/rest_api/__init__.py
+++ app/master/web_server/rest_api/__init__.py
@@ -1,7 +1,7 @@
 from flask import request
 from master.web_server import master_app, data
 from master.web_server.response_helpers import *
-from master.db.models import Session, Job
+from master.db.models import Session, Job, Task
 import sqlalchemy.exc
 
 @master_app.route('/api/job/', methods=['GET', 'POST'])
@@ -11,34 +11,34 @@ def job(job_id=None):
         session = Session()
         if job_id:
             job = session.query(Job).filter(Job.id == job_id).first()
-            return job.to_json() if job else (not_found(), 404)
+            r = job.to_json() if job else (not_found(), 404)
+            session.close()
+            return r
         else:
             jobs = session.query(Job).all()
-            return json_out(jobs) if jobs else (not_found(), 404)
+            r = json_out(jobs) if jobs else (not_found(), 404)
+            session.close()
+            return r
     elif request.method == 'POST':
         session = Session()
-        # try:
-        j = Job(**request.get_json())
-        session.add(j)
-        session.commit()
-        job=j.to_json()
-        session.close()
-        #add operation to queue
-        data['job_big_operation_controller'].add(j)
-        return job
-        # except sqlalchemy.exc.IntegrityError:
-        #     return bad_request(), 400
-        # except:
-        #     return internal_error(), 500
+        try:
+            j = Job(**request.get_json())
+            session.add(j)
+            session.commit()
+            job=j.to_json()
+            session.close()
+            #add operation to queue
+            data['job_big_operation_controller'].add(j)
+            return job
+        except sqlalchemy.exc.IntegrityError:
+            return bad_request(), 400
+        except:
+            return internal_error(), 500
 
 @master_app.route('/api/job/<string:job_id>/tasks/', methods=['GET'])
 def task(job_id=None):
     session = Session()
     tasks = session.query(Task).filter(Task.job_id == job_id).all()
-    if tasks:
-        tasks = json_out(tasks)
-        session.close()
-        return tasks
-    else:
-        session.close()
-        return not_found(), 404
+    r = json_out(tasks) if tasks else (not_found(), 404)
+    session.close()
+    return r

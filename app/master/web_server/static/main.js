451d40845c4e18f5f089daf224a2d177e9a919d9
Michael O'Neill <michaeloneill94@live.ie>
2015-12-02 18:02:32 +0000

    Added view tasks ui with refresh for seeing a jobs tasks

--- app/master/web_server/static/main.js
+++ app/master/web_server/static/main.js
@@ -3,12 +3,13 @@ $(document).ready(function() {
   /* ---- HANDLEBARS STUFF ----- */
   //compile handlebars templates
   JOBS_TABLE = Handlebars.compile($("#jobs-table-mkup").html());
+  TASKS_TABLE = Handlebars.compile($("#tasks-table-mkup").html());
   CREATE_JOB_FORM = $("#create-job-form-mkup").html();
   HORISONTAL_LOADER = "<img src='/static/horisontal-loader.gif' alt='small loader' />";
 
   //HANDLEBARS HELPERS
   Handlebars.registerHelper("isExecuting", function(status, options) {
-		if(status.toLowerCase() == "executing") {
+    if(status.toLowerCase() == "executing") {
 			return options.fn(this);
 		}
 		else {
@@ -17,6 +18,14 @@ $(document).ready(function() {
 	});
   Handlebars.registerHelper("isCompleted", function(status, options) {
 		if(status.toLowerCase() == "completed") {
+			return options.fn(this);
+		}
+		else {
+			return options.inverse(this);
+		}
+	});
+  Handlebars.registerHelper("isFailed", function(status, options) {
+		if(status.toLowerCase() == "failed") {
 			return options.fn(this);
 		}
 		else {
@@ -32,13 +41,33 @@ $(document).ready(function() {
   $(document).on("click", ".nav li.view-jobs a", view_jobs_ui);
   $(document).on("click", ".nav li.create-job a", create_job_ui);
 
+  //view listeners
   $(document).on("submit", "#create-job", create_job_submit);
+  $(document).on("click", ".view-tasks", view_tasks_ui);
+
+  //refresh listeners
+  $(document).on("click", ".tasks-reload",view_tasks_ui);
   /* ----- END LISTENERS ------ */
 
 });
 
 /* ---- UI View Functions ---- */
+function view_tasks_ui() {
+  clearPanel();
+  job_id = $(this).data("job-id");
+  $("#panel-title").text("Tasks for Job "+job_id);
+  $("#panel-title").append('<i class="glyphicon glyphicon-refresh box-reload tasks-reload" data-job-id="'+job_id+'"></i>');
+  $.when(get_tasks(job_id)).done(function(tasks) {
+    $("#panel-body").empty().append(TASKS_TABLE({tasks: tasks}));
+  }).fail(function(jqXHR, textStatus, errorThrown) {
+    if(jqXHR.status) {
+      $("#panel-body").empty().append("<p>There a no tasks for this job yet.</p>");
+    }
+  });
+}
+
 function view_jobs_ui() {
+  clearPanel();
   $("#panel-title").text("Jobs");
   $(".nav li").removeClass("active");
   $(".nav li.view-jobs").addClass("active");
@@ -46,7 +75,7 @@ function view_jobs_ui() {
     $("#panel-body").empty().append(JOBS_TABLE({jobs: jobs}));
   }).fail(function(jqXHR, textStatus, errorThrown) {
     if(jqXHR.status) {
-      $("#panel-body").empty().append("<p>There a no jobs.</p>");
+      $("#panel-body").empty().append("<p>There are no jobs.</p>");
     }
   });
 }
@@ -59,6 +88,11 @@ function create_job_ui() {
 }
 
 /* ---- END UI View Functions ---- */
+/* ---- UI Helper Functions ---- */
+function clearPanel() {
+  $("#panel-body").empty().append("<p>Loading...</p>");
+}
+/* ---- END UI Helper Functions ---- */
 
 /* --- Listener Binded Fucntions ---- */
 function create_job_submit() {
@@ -82,6 +116,10 @@ function create_job_submit() {
 /* --- END Listener Binded Fucntions ---- */
 
 /* ---- API Calls ---- */
+function get_tasks(job_id) {
+    return doGet("job/"+job_id+"/tasks/");
+}
+
 function get_jobs() {
     return doGet("job/");
 }

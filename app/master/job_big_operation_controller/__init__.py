8dbb7292d8f41dc8979bd13c5542152fc08d0464
Michael O'Neill <michaeloneill94@live.ie>
2016-04-13 09:14:15 +0000

    Trying this

--- app/master/job_big_operation_controller/__init__.py
+++ app/master/job_big_operation_controller/__init__.py
@@ -15,6 +15,7 @@ class JobBigOperationController(threading.Thread):
         #queue of jobs to perform operations on
         self.queue = Queue()
         self.async_downloader = async_downloader
+        self.session = Session()
 
     def add(self, job):
         log.info('Queuing Big Job Operation  for %s' % (job))
@@ -25,14 +26,15 @@ class JobBigOperationController(threading.Thread):
         while True:
             job = self.queue.get()
             log.info('Running Big Operation for %s' % (job))
-            session = Session()
             if hasattr(job, 'output_log') and job.output_log:
-                job.set_session(session)
+                job.set_session(self.session)
                 job.output_log_to_s3()
             #Job in created state needs to be submitted
             elif job.status == 'created':
-                job.set_session(session)
+                job.set_session(self.session)
                 job.submit()
+                #TRY
+                # job.session.rollback()
                 data_preparer = JobDataPreparer(job)
                 self.async_downloader.pause()
                 data_preparer.prepare_data()
@@ -40,7 +42,7 @@ class JobBigOperationController(threading.Thread):
 
             #Job in tasks completed needs to have it's final script executed
             elif job.status == 'tasks completed':
-                job.set_session(session)
+                job.set_session(self.session)
                 #pause async_downloader
                 job.downloaded_task_outputs = self.async_downloader.pause(job_id=job.id)
                 print 'DOWNLOADED: '+str(job.downloaded_task_outputs)
@@ -49,5 +51,4 @@ class JobBigOperationController(threading.Thread):
             else:
                 #raise some exception
                 log.error('%s shouldn\'t be here' % (job))
-            session.close()
             time.sleep(0.01)
